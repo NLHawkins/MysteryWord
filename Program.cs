38ed187ddb86f9889766ef3be40af60010baa108
NLHawkins <nathanlhawkins@gmail.com>
2017-01-12 17:39:44 -0500

    Thurs Night draft, ready to split string into char.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,37 @@ namespace MysteryWord
 {
     class Program
     {
-        static void Main(string[] args)
+        static void DrawBoard(List<string> letterList)
         {
+            foreach (string letter in letterList)
+                Console.WriteLine("_ ");
+        }
+
+       
+
+
+        
+            
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Let's Play Mystery Word");
+
+             
+            string[] wordArray;
+            wordArray = File.ReadAllLines(@"..\..\WordList.txt");
+            List<string> wordList = wordArray.ToList<string>();
+            var rwg = new Random();
+            int rndmNumb = rwg.Next(wordList.Count);
+
+            string mystWord = (wordList[rndmNumb]);
+            Console.WriteLine(mystWord);
+
+            var mystWordCharList = mystWord.Split("");
+
+
+
+
+
         }
     }
 }
